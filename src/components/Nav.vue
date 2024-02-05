3b6436882998bc8514dd527a50d4e837b3215f1e
melinaskye <melinaskye@optonline.net>
2024-02-05 15:21:56 -0500

    changing router links in the nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="flex justify-end">
         <nav v-if="user">
-            <router-link to="/admin" class="text-yellow-600">Admin</router-link> |
+            <!-- <router-link to="/admin" class="text-yellow-600">Admin</router-link> | -->
             <router-link to="/logout" @click="logout" class="text-yellow-600">Logout</router-link>
             <!-- <img src="../assets/mikan-circle.png" alt="fudeko mikan logo" class="inline w-4 pt-5 mx-auto md:w-8"> -->
         </nav>
         <nav v-else>
-            <router-link to="/" class="text-yellow-600">Home</router-link> |
+            <!-- <router-link to="/" class="text-yellow-600">Home</router-link> | -->
             <router-link to="/login" class="text-yellow-600">Login</router-link>
             <!-- <img src="../assets/mikan-circle.png" alt="fudeko mikan logo" class="inline w-4 pt-5 mx-auto md:w-8"> -->
         </nav>
