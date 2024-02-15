e20623b659308d4a7c0b8bd2faed8bdc285f9732
Tom Lee <jcblee@gmail.com>
2024-02-15 15:41:21 -0500

    form options fixes

--- src/components/ResponseForm.vue
+++ src/components/ResponseForm.vue
@@ -52,7 +52,7 @@
 
                         <div v-if="attrOption.name == 'CreditWithGivenName'"> You will be credited as {{
                             participantRecord.first_name + ' ' + participantRecord.last_name }}</div>
-                        <div v-if="attrOption.name == 'CreditDifferent'">
+                        <div v-if="attrOption.name == 'CreditDifferent' && shareOption != shareSettings[0]">
                             <label for="alt-credit" class="mr-2">Enter name to be credited as:</label>
                             <input id="alt-credit" type="text" v-model="creditName" class="p-2 border-2 rounded-md">
                         </div>
@@ -83,6 +83,8 @@ import WritingTip from '@/components/WritingTip.vue'
 // import Confirmation from './Confirmation.vue'
 import Editor from 'primevue/editor'
 import Dropdown from 'primevue/dropdown'
+
+const shareSettingsArray = [{ name: "Keep Private", description: "You can always opt to share your response later if you change your mind. You can still share your responses with your friends and family on your private page if you want." }, { name: "Share with Storytellers", description: "Your words may be just the thing to jog another storyteller’s memory. Choose this option if you are okay with sharing this response with other storytellers in Fudeko email and letter correspondence. The response will not be made publicly available. Still, while we discourage storytellers from forwarding Fudeko emails outside the group, we cannot totally prevent it. Thank you for helping to build a creative and collaborative storytelling community!" }, { name: "Release with Creative Commons License", description: " <a class='blue-200' href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>(CC-BY-NC-SA 4.0 DEED)</a> Want to make this response publicly available for education or research? This license allows others to use, excerpt, or adapt your response as long as they 1) give proper attribution, 2) do not profit from it, and 3) release the resulting project under the same license. While a CC license cannot be retroactively revoked, we can remove the response(s) from our website. However, if they have been shared elsewhere, you will have to negotiate with those parties if you wish to have them taken down. If you have concerns/doubts, we encourage you to keep the response private or only share it with the group for now." }, { name: "Release Anonymously with Creative Commons License", description: "(CC-BY-NC-SA 4.0 DEED) Choose this to make this response anonymously available to the public for education or research purposes. While still released under the same CC license, proper attribution in this case is “by Anonymous, courtesy of the Fudeko Project.” If you decide you would like to be named with the response later, just email us at hana@fudekoproject.org and we will update the attribution information accordingly" }];
 export default {
     components: {
         WritingTip,
@@ -93,11 +95,11 @@ export default {
         return {
             response: "",
             userTitle: "",
-            shareSettings: [{ name: "Keep Private", description: "You can always opt to share your response later if you change your mind. You can still share your responses with your friends and family on your private page if you want." }, { name: "Share with Storytellers", description: "Your words may be just the thing to jog another storyteller’s memory. Choose this option if you are okay with sharing this response with other storytellers in Fudeko email and letter correspondence. The response will not be made publicly available. Still, while we discourage storytellers from forwarding Fudeko emails outside the group, we cannot totally prevent it. Thank you for helping to build a creative and collaborative storytelling community!" }, { name: "Release with Creative Commons License", description: " <a class='blue-200' href='https://creativecommons.org/licenses/by-nc-sa/4.0/'>(CC-BY-NC-SA 4.0 DEED)</a> Want to make this response publicly available for education or research? This license allows others to use, excerpt, or adapt your response as long as they 1) give proper attribution, 2) do not profit from it, and 3) release the resulting project under the same license. While a CC license cannot be retroactively revoked, we can remove the response(s) from our website. However, if they have been shared elsewhere, you will have to negotiate with those parties if you wish to have them taken down. If you have concerns/doubts, we encourage you to keep the response private or only share it with the group for now." }, { name: "Release Anonymously with Creative Commons License", description: "(CC-BY-NC-SA 4.0 DEED) Choose this to make this response anonymously available to the public for education or research purposes. While still released under the same CC license, proper attribution in this case is “by Anonymous, courtesy of the Fudeko Project.” If you decide you would like to be named with the response later, just email us at hana@fudekoproject.org and we will update the attribution information accordingly" }],
+            shareSettings: shareSettingsArray,
             attrSettings: [{ name: "RemainAnonymous", description: "Remain Anonymous" }, { name: "CreditWithGivenName", description: "Credit me as my registered name" }, { name: "CreditDifferent", description: "Credit me as a different name" },],
             difficulty: "",
             attrOption: "",
-            shareOption: "",
+            shareOption: shareSettingsArray[0],
             hasUnansweredSets: true,
             creditName: "",
             difficultyOptions: ["Easy", "Moderate", "Somewhat Difficult", "Difficult"],
@@ -125,6 +127,7 @@ export default {
             this.file = e.target.files[0]
         },
         async submit() {
+            if( this.submitButtonDisabled ) return;
             this.toggleLoading();
             const bodyData = {
                 response_text: this.response,
@@ -146,7 +149,7 @@ export default {
         ...mapStores( useCounterStore ),
         ...mapState( useCounterStore, ['loading', 'error', 'usersPromptChoices', 'tips', 'participantID', 'participantRecord'] ),
         submitButtonDisabled() {
-            return ( this.shareOption.description == "" || this.response == "" || !this.shareOption.name || !this.difficulty || !this.attrOption.name || this.attrOption.name == "CreditDifferent" && this.creditName == "" )
+            return this.response.length < 1  || !this.shareOption || this.shareOption.name != "Keep Private" && !this.attrOption
         },
         custom() {
             return this.$route.path.includes( 'custom' )
