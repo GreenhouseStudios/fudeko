c0802192f5176391b8e872c2c0e84885fc830241
Tom Lee <jcblee@gmail.com>
2024-04-04 12:56:34 -0400

    confirm decline component

---
+++ src/components/ConfirmDecline.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        Confirm Decline?
+        <Button @click="confirmDecline" class="p-2 my-2 bg-red-300 border-2 border-red-400">Confirm</Button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            }
+        },
+        methods: {
+            
+        },
+        components: {
+            
+        },
+        mounted() {
+
+        },
+        computed: {
+            
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
