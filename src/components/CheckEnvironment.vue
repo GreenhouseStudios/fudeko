f1e5aea96023a61dd78e4023b15be971a66d2029
SamChichester <lnsschi1225@gmail.com>
2025-04-04 13:19:46 -0400

    Added filtering if env variable is not set.

--- src/components/CheckEnvironment.vue
+++ src/components/CheckEnvironment.vue
@@ -10,15 +10,16 @@ export default {
   methods: {
     checkEnvironment() {
       const supabaseUrl = import.meta.env.VITE_APP_SUPABASE_URL || "";
-      return supabaseUrl.includes("cfgiqouvnfofhciztbri");
+      const devCode = import.meta.env.VITE_APP_DEV_CODE || "";
+      return !(devCode == "") && supabaseUrl.includes(devCode);
     }
   }
 }
 </script>
 
 <template>
-  <div class="fixed bottom-5 right-5 z-50 w-64 p-2">
-    <p v-if="isDev">Development</p>
+  <div class="fixed bottom-5 right-5 z-50 w-64 p-2" v-if="isDev">
+    <p>Development</p>
     <small>Commit: {{ commitHash }}</small><br>
     <small>Message: {{ commitMessage }}</small>
   </div>
